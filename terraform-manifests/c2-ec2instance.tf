313527df154fc46dd1ac0c3333d67c24b5e6ea03
Osang123 <osangmow@gmail.com>
2024-01-14 06:48:53 -0800

    update tagging

--- terraform-manifests/c2-ec2instance.tf
+++ terraform-manifests/c2-ec2instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "myec2vm" {
   associate_public_ip_address = true
   user_data              = file("${path.module}/app1-install.sh")
   tags = {
-    "Name" = sanderin_tag
+    "Name" = osang_tag
     
   }
 }
