cf6549bba0b8570582601eddd1204ccdb6af7d76
7benzy <junior7benzy@gmail.com>
2024-01-14 08:53:23 -0500

    name tag

--- terraform-manifests/c2-ec2instance.tf
+++ terraform-manifests/c2-ec2instance.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "myec2vm" {
   associate_public_ip_address = true
   user_data              = file("${path.module}/app1-install.sh")
   tags = {
-    "Name" = "EC2 Demo"
+    "Name" = "EC2 Github"
   }
 }
