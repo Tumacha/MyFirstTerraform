9ce6398cf84714fa239ce508702f54d143c35528
7benzy <junior7benzy@gmail.com>
2024-01-14 08:21:09 -0500

    update instance type

--- terraform-manifests/c2-ec2instance.tf
+++ terraform-manifests/c2-ec2instance.tf
@@ -1,7 +1,7 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami                    = "ami-079db87dc4c10ac91"
-  instance_type          = "t3.large"
+  instance_type          = "t3.xlarge"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
